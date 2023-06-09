dd757362732a137bb82ebc37419e2cf73b5c2467
Nguyen giang nam <namng050185@gmail.com>
2023-06-09 22:24:23 +0700

    Cấu hình ẩn playground cũ và thay bằng hiển thị ApolloServer sandbox

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { FooResolver } from './foo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      //autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      //sortSchema: true,
+      installSubscriptionHandlers: true,
+      autoSchemaFile: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FooResolver],
 })
 export class AppModule {}
