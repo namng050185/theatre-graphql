21162983e1baa0924bdbecbd73ac080e0d6b291e
Nguyen giang nam <namng050185@gmail.com>
2023-06-28 21:32:28 +0700

    Co-authored-by: Nguyen giang nam <namng050185@gmail.com>

---
+++ deploy-prod.sh
@@ -0,0 +1,5 @@
+docker build -t namng050185/nest-api:production . --file Dockerfile.dev
+
+docker login --username namng050185 --password dckr_pat_5hivzlrt7GRkv_E5S6YXHwxQNJo
+
+docker push namng050185/nest-api:production 
